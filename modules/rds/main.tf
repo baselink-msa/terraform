e43377e283887671aefefce77fa15ac728d683f9
minsssssd <mintx0526@naver.com>
2026-05-19 14:45:35 +0900

    rds

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,6 +1,19 @@
-###############################################################################
-# modules/rds/main.tf
-#
-# TODO: rds 모듈 담당자가 리소스를 작성하세요.
-#       (의도적으로 비워둔 스텁 파일입니다)
-###############################################################################
+resource "aws_db_instance" "this" {
+  identifier        = var.identifier
+  engine            = "postgres"
+  engine_version    = var.engine_version
+  instance_class    = var.instance_class
+  allocated_storage = var.allocated_storage
+
+  db_name  = var.db_name
+  username = var.username
+  password = var.password # Apply 시 외부에서 주입받음
+
+  vpc_security_group_ids = var.vpc_security_group_ids
+  db_subnet_group_name   = var.db_subnet_group_name
+
+  skip_final_snapshot = var.skip_final_snapshot
+  publicly_accessible = var.publicly_accessible
+
+  tags = var.tags
+}
