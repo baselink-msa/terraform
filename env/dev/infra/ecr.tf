714bface170232bd0a7cc470f665e0a51aa75191
yealims <yealim.n@gmail.com>
2026-05-19 15:18:26 +0900

    refactor: tfvars 적용

--- env/dev/infra/ecr.tf
+++ env/dev/infra/ecr.tf
@@ -1,17 +1,6 @@
 module "ecr" {
   source = "../../../modules/ecr"
 
-  environment = "dev"
-  
-  repositories = [
-    "auth-service",
-    "game-service",
-    "waiting-room-service",
-    "seat-lock-service",
-    "ticket-service",
-    "ticket-worker-service",
-    "order-service",
-    "ai-chatbot-service",
-    "admin-service"
-  ]
+  environment  = var.ecr_environment
+  repositories = var.ecr_repositories
 }
